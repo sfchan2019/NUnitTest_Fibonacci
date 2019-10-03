2d6b7ad60531ddc5f16673a8ba2d666c45467522
DESKTOP-1ORJFAV\Sing <sing840722@gmail.com>
2019-10-03 23:54:20 +0100

    NUnit with Sum of even values from Fib numbers

---
+++ EvenFibonacciNumbers.Test/EvenFibonacciNumbersTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using EvenFibonacciNumbers;
+
+namespace EvenFibonacciNumbers.Test
+{
+    [TestFixture()]
+    class EvenFibonacciNumbersTest
+    {
+        FibonacciNumber fn = new FibonacciNumber();
+
+        //[Test()]
+        //public void FibNumTest()
+        //{
+        //    Assert.AreEqual(2, fn.FibNum(3));
+        //    Assert.AreEqual(1548008755920, fn.FibNum(60));
+        //    Assert.Greater(4000000, fn.FibNum(33));
+        //    Assert.Less(4000000, fn.FibNum(34));
+        //}
+
+        [Test()]
+        public void EvenSumTest()
+        {
+            Assert.AreNotEqual(2, fn.SumEven(6));
+            Assert.AreEqual(44, fn.SumEven(9));
+        }
+    }
+}
---
+++ EvenFibonacciNumbers/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacciNumbers
+{
+    public class FibonacciNumber
+    {
+        //    public long FibNum(int n)
+        //    {
+        //        if (n == 1 || n == 2)
+        //        {
+        //            return 1;
+        //        }
+        //        long[] memo = new long[n + 1];
+        //        memo[1] = 1;
+        //        memo[2] = 1;
+        //        for (int i = 3; i <= n; i++)
+        //        {
+        //            memo[i] = memo[i - 1] + memo[i - 2];
+        //        }
+        //        return memo[n];
+        //    }
+
+        public long SumEven(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            long sum = 0;
+            long[] memo = new long[n + 1];
+            memo[1] = 1;
+            memo[2] = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                memo[i] = memo[i - 1] + memo[i - 2];
+                if((i) % 3 == 0)
+                sum += memo[i];
+            }
+            return sum;
+        }
+    }
+}
